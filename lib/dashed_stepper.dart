be3aa4a6ef35d7d9fda5967d5ce1b48ce85d8f29
Mahmoud nabil <mahmoudnabil141981@gmail.com>
2024-01-24 13:32:15 +0200

    - remove gap in disabled line - equal lines width

--- lib/dashed_stepper.dart
+++ lib/dashed_stepper.dart
@@ -5,8 +5,8 @@ import 'package:flutter/material.dart';
 class DashedStepper extends StatelessWidget {
   const DashedStepper({
     super.key,
-    this.length = 3,
-    this.step = 0,
+    this.steps = 3,
+    this.currentStep = 0,
     this.icons,
     this.height,
     this.labelStyle,
@@ -14,11 +14,14 @@ class DashedStepper extends StatelessWidget {
     this.disabledColor,
     this.lineHeight,
     this.dotSize,
-    this.gap, required this.activeWidget, required this.inActiveWidget, required this.finishedWidget,
-  }) : assert((icons == null || icons.length == length), 'icons length must be the same as length');
+    required this.activeWidget,
+    required this.inActiveWidget,
+    required this.finishedWidget,
+  }) : assert((icons == null || icons.length == steps),
+            'icons length must be the same as length');
 
-  final int length;
-  final int step;
+  final int steps;
+  final int currentStep;
   final List<Widget>? icons;
   final double? height;
   final TextStyle? labelStyle;
@@ -26,7 +29,6 @@ class DashedStepper extends StatelessWidget {
   final Color? disabledColor;
   final double? lineHeight;
   final double? dotSize;
-  final double? gap;
   final Widget activeWidget;
   final Widget inActiveWidget;
   final Widget finishedWidget;
@@ -37,7 +39,7 @@ class DashedStepper extends StatelessWidget {
       return Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: List.generate(
-          length,
+          steps,
           (index) {
             return Expanded(
               child: Column(
@@ -50,24 +52,26 @@ class DashedStepper extends StatelessWidget {
                       child: icons![index],
                     ),
                   _HorizStep(
-                    gap: gap,
                     dotSize: dotSize,
                     height: lineHeight,
                     activeColor: indicatorColor,
                     inActiveColor: disabledColor,
-                    length: size.maxWidth / length,
-                    left: index < step,
-                    right: index < step - 1,
+                    stepLinesWidth: size.maxWidth / steps,
+                    left: index < currentStep,
+                    right: index < currentStep - 1,
                     roundedLeft: index == 0
                         ? true
-                        : index < step
+                        : index < currentStep
                             ? false
                             : true,
-                    roundedRight: index == length - 1
+                    roundedRight: index == steps - 1
                         ? true
-                        : index < step - 1
+                        : index < currentStep - 1
                             ? false
-                            : true, activeWidget: activeWidget, inActiveWidget: inActiveWidget, finishedWidget: finishedWidget,
+                            : true,
+                    activeWidget: activeWidget,
+                    inActiveWidget: inActiveWidget,
+                    finishedWidget: finishedWidget,
                   ),
                 ],
               ),
@@ -83,14 +87,13 @@ class _HorizStep extends StatelessWidget {
   const _HorizStep({
     this.left = false,
     this.right = false,
-    this.length = 100,
+    this.stepLinesWidth = 100,
     this.roundedLeft = true,
     this.roundedRight = true,
     this.activeColor,
     this.inActiveColor,
     this.height,
     this.dotSize,
-    this.gap,
     required this.activeWidget,
     required this.inActiveWidget,
     required this.finishedWidget,
@@ -98,7 +101,7 @@ class _HorizStep extends StatelessWidget {
 
   final bool left;
   final bool right;
-  final double length;
+  final double stepLinesWidth;
   final bool roundedLeft;
   final bool roundedRight;
   final Color? activeColor;
@@ -108,7 +111,6 @@ class _HorizStep extends StatelessWidget {
   final Color? inActiveColor;
   final double? height;
   final double? dotSize;
-  final double? gap;
 
   @override
   Widget build(BuildContext context) {
@@ -119,32 +121,29 @@ class _HorizStep extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             _Line(
-              width: length / 2,
+              width: stepLinesWidth / 2,
               isActive: left,
               roundedLeft: roundedLeft,
               color: activeColor,
               disabledColor: inActiveColor,
               height: height,
-              gap: gap,
             ),
             _Line(
-              width: length / 2,
+              width: stepLinesWidth / 4,
               isActive: right,
               roundedRight: roundedRight,
               color: activeColor,
               disabledColor: inActiveColor,
               height: height,
-              gap: gap,
             ),
           ],
         ),
-        if(left==true && right == false)
+        if (left == true && right == false)
           activeWidget
-        else if(left&&right)
+        else if (left && right)
           finishedWidget
         else
           inActiveWidget
-
       ],
     );
   }
@@ -159,7 +158,6 @@ class _Line extends StatelessWidget {
     this.isActive = true,
     this.roundedLeft = true,
     this.roundedRight = true,
-    this.gap,
   });
 
   final double width;
@@ -169,41 +167,19 @@ class _Line extends StatelessWidget {
   final bool isActive;
   final bool roundedLeft;
   final bool roundedRight;
-  final double? gap;
 
   @override
   Widget build(BuildContext context) {
-    if (isActive) {
-      return Container(
-        width: width,
-        height: height ?? 4,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.horizontal(
-            left: roundedLeft ? const Radius.circular(50) : Radius.zero,
-            right: roundedRight ? const Radius.circular(50) : Radius.zero,
-          ),
-          color: color ?? Colors.blue,
+    return Container(
+      width: width,
+      height: height ?? 4,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.horizontal(
+          left: roundedLeft ? const Radius.circular(50) : Radius.zero,
+          right: roundedRight ? const Radius.circular(50) : Radius.zero,
         ),
-      );
-    } else {
-      return Row(
-        children: List.generate(
-          3,
-          (index) => Container(
-            margin: EdgeInsets.only(left: gap ?? 2),
-            width: width / 3 - (gap ?? 2),
-            height: height ?? 4,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.horizontal(
-                left: roundedLeft ? const Radius.circular(50) : Radius.zero,
-                right: roundedRight ? const Radius.circular(50) : Radius.zero,
-              ),
-              color: disabledColor ?? Colors.grey[300],
-            ),
-          ),
-        ),
-      );
-    }
+        color: isActive ? color : disabledColor,
+      ),
+    );
   }
 }
-
